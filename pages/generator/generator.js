90d5166f7ada56cd2cf3491bd8435f86e5b34962
Roman sbmaxx Rozhdestvenskiy <sbmaxx@yandex-team.ru>
2014-07-14 10:29:32 +0400

    generator

---
+++ pages/generator/generator.js
@@ -0,0 +1,57 @@
+var fs = require('fs'),
+    inlineJS = fs.readFileSync('./blocks/page/page__inline.js', { encoding: 'utf8' }),
+    borschik = new (require('borschik/lib/techs/js').Tech)({ techOptions: {} });
+
+var favicons = {
+    ru: '//yastatic.net/morda-logo/i/favicon_islands.ico',
+    en: '//yastatic.net/morda-logo/i/favicon_comtr.ico'
+};
+
+module.exports = function(data) {
+
+    var data = data || require('../../data.js'),
+        lang = data.order[0],
+        title = 'ololo: ' + data.cards[lang].name;
+
+    return  {
+        block: 'page',
+        title: title,
+        favicon: favicons[lang],
+        head: [
+            { elem: 'css', url: '_index.css' },
+            {
+                elem: 'meta',
+                attrs: {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1'
+                }
+            },
+            {
+                elem: 'js',
+                content: borschik.minimize(inlineJS)
+            },
+            {
+                elem: 'meta',
+                attrs: {
+                    name: 'format-detection',
+                    content: 'telephone=no'
+                }
+            },
+            {
+                elem: 'js',
+                url: '//yastatic.net/jquery/2.1.1/jquery.min.js'
+            },
+            {
+                elem: 'js',
+                url: '_index.js'
+            }
+        ],
+        content: {
+            block: 'card',
+            order: data.order,
+            cards: data.cards,
+            favicons: favicons
+        }
+    };
+
+};
--- pages/index/index.bemjson.js
+++ pages/index/index.bemjson.js
@@ -3,51 +3,6 @@ var fs = require('fs'),
     borschik = new (require('borschik/lib/techs/js').Tech)({ techOptions: {} });
 
 var data = require('../../data.js'),
-    lang = data.order[0],
-    title = data.cards[lang].name;
+    generator = require('../generator/generator.js');
 
-var favicons = {
-    ru: '//yastatic.net/morda-logo/i/favicon_islands.ico',
-    en: '//yastatic.net/morda-logo/i/favicon_comtr.ico'
-};
-
-module.exports = {
-    block: 'page',
-    title: title,
-    favicon: favicons[lang],
-    head: [
-        { elem: 'css', url: '_index.css' },
-        {
-            elem: 'meta',
-            attrs: {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1'
-            }
-        },
-        {
-            elem: 'js',
-            content: borschik.minimize(inlineJS)
-        },
-        {
-            elem: 'meta',
-            attrs: {
-                name: 'format-detection',
-                content: 'telephone=no'
-            }
-        },
-        {
-            elem: 'js',
-            url: '//yastatic.net/jquery/2.1.1/jquery.min.js'
-        },
-        {
-            elem: 'js',
-            url: '_index.js'
-        }
-    ],
-    content: {
-        block: 'card',
-        order: data.order,
-        cards: data.cards,
-        favicons: favicons
-    }
-};
+module.exports = generator(data);
