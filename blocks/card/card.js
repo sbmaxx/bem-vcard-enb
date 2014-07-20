efa672d20ee8f0593fa726dbe3607225de6ea465
Roman Rozhdestvenskiy <sbmaxx@gmail.com>
2014-07-20 10:02:17 +0400

    console

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -147,3 +147,12 @@ var Card = (function() {
     }
 
 }());
+
+if (typeof console !== 'undefined') {
+    var css = 'font-size:18px; \
+    font-family:consolas; \
+    line-height:50px; \
+    margin:15px; \
+    padding:10px; display: inline-block;'
+    console.log('%c https://github.com/sbmaxx/bem-vcard-enb', css);
+}
