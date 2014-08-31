96552a563edaf2b9f35388f713aa60298169da31
sbmaxx <sbmaxx@gmail.com>
2014-08-31 11:49:45 +0400

    restored bemdecl

---
+++ pages/index/index.bemdecl.js
@@ -0,0 +1,23 @@
+exports.deps = [
+    {
+        "block": "page"
+    },
+    {
+        "block": "page",
+        "elem": "meta"
+    },
+    {
+        "block": "page",
+        "elem": "js"
+    },
+    {
+        "block": "page",
+        "elem": "css"
+    },
+    {
+        "block": "card"
+    },
+    {
+        "block": "metrika"
+    }
+];
