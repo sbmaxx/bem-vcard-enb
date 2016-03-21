14377dad928ccef240e715a7adc1e5d3491a0d82
miripiruni <miripiruni@gmail.com>
2016-03-21 15:08:10 +0300

    Don’t repean block subpredicate

--- blocks/card/card.bemhtml.js
+++ blocks/card/card.bemhtml.js
@@ -72,265 +72,265 @@ block('card')(
         }
 
         return content;
-    })
-);
-
-block('card').elem('content').content()(function() {
-    var data = this.ctx.data;
-
-    return [
-        {
-            elem: 'logo',
-            lang: data.lang,
-            site: data.company.site,
-            name: data.company.name
-        },
-        {
-            elem: 'text',
-            content: [
-                {
-                    elem: 'title',
-                    data: {
-                        name: data.name,
-                        position: data.position
-                    },
-                    lang: data.lang
-                },
-                {
-                    elem: 'address',
-                    data: data.address,
-                    lang: data.lang
-                },
-                {
-                    elem: 'contact',
-                    data: data,
-                    lang: data.lang
-                },
-                {
-                    elem: 'extra',
-                    data: data.extra,
-                    lang: data.lang
-                }
-            ]
-        }
-    ];
-});
-
-block('card').elem('title').content()(function() {
-    return [
-        {
-            elem: 'name',
-            content: this.ctx.data.name
-        },
-        {
-            elem: 'position',
-            content: this.ctx.data.position
-        }
-    ];
-});
-
-block('card').elem('name')(
-    tag()('h1'),
-    attrs()({
-        itemprop: 'name'
-    })
-);
-
-block('card').elem('position').attrs()({
-    itemprop: 'title'
-});
-
-block('card').elem('address')(
-    attrs()(function() {
-        return {
-            itemprop: 'address',
-            itemscope: true,
-            itemtype: 'http://data-vocabulary.org/Address'
-        };
     }),
-    content()(function() {
-        var ctx = this.ctx;
 
-        if (typeof ctx.data === 'string') {
-            return ctx.data;
+    elem('content').content()(function() {
+        var data = this.ctx.data;
+
+        return [
+            {
+                elem: 'logo',
+                lang: data.lang,
+                site: data.company.site,
+                name: data.company.name
+            },
+            {
+                elem: 'text',
+                content: [
+                    {
+                        elem: 'title',
+                        data: {
+                            name: data.name,
+                            position: data.position
+                        },
+                        lang: data.lang
+                    },
+                    {
+                        elem: 'address',
+                        data: data.address,
+                        lang: data.lang
+                    },
+                    {
+                        elem: 'contact',
+                        data: data,
+                        lang: data.lang
+                    },
+                    {
+                        elem: 'extra',
+                        data: data.extra,
+                        lang: data.lang
+                    }
+                ]
+            }
+        ];
+    }),
+
+    elem('title').content()(function() {
+        return [
+            {
+                elem: 'name',
+                content: this.ctx.data.name
+            },
+            {
+                elem: 'position',
+                content: this.ctx.data.position
+            }
+        ];
+    }),
+
+    elem('name')(
+        tag()('h1'),
+        attrs()({
+            itemprop: 'name'
+        })
+    ),
+
+    elem('position').attrs()({
+        itemprop: 'title'
+    }),
+
+    elem('address')(
+        attrs()(function() {
+            return {
+                itemprop: 'address',
+                itemscope: true,
+                itemtype: 'http://data-vocabulary.org/Address'
+            };
+        }),
+        content()(function() {
+            var ctx = this.ctx;
+
+            if (typeof ctx.data === 'string') {
+                return ctx.data;
+            }
+
+            var order = ctx.data.lang === 'ru'
+                ? ['country', 'city', 'zip', 'street-address']
+                : ['city', 'zip', 'country', 'street-address'];
+
+            var content = [];
+
+            order.forEach(function(el, i) {
+                content.push({
+                    elem: el,
+                    content: ctx.data[el]
+                });
+                if (i !== order.length - 1) {
+                    content.push(', ');
+                }
+            });
+
+            return content;
+        })
+    ),
+
+    elem('country')(
+        tag()('span'),
+        attrs()({
+            itemprop: 'country-name'
+        })
+    ),
+
+    elem('city')(
+        tag()('span'),
+        attrs()({
+            itemprop: 'locality'
+        })
+    ),
+
+    elem('zip')(
+        tag()('span'),
+        attrs()({
+            itemprop: 'postal-code'
+        })
+    ),
+
+    elem('street-address')(
+        tag()('span'),
+        attrs()({
+            itemprop: 'street-address'
+        })
+    ),
+
+    elem('contact').content()(function() {
+        var content = [];
+        var data = this.ctx.data.contact;
+
+        if (data.work) {
+            content.push({
+                elem: 'tel',
+                elemMods: { type: 'work' },
+                content: [
+                    i18n[this.ctx.lang].tel,
+                    {
+                        elem: 'phone-link',
+                        raw: data.work.replace(/\(|\)|\s|\-/g, ''),
+                        content: data.work
+                    },
+                    data.workExt
+                        ? i18n[this.ctx.lang].telExt + data.workExt
+                        : ''
+                ]
+            });
         }
 
-        var order = ctx.data.lang === 'ru'
-            ? ['country', 'city', 'zip', 'street-address']
-            : ['city', 'zip', 'country', 'street-address'];
-
-        var content = [];
-
-        order.forEach(function(el, i) {
+        if (data.cell) {
             content.push({
-                elem: el,
-                content: ctx.data[el]
+                elem: 'tel',
+                elemMods: { type: 'cellular' },
+                content: [
+                    i18n[this.ctx.lang].cell,
+                    {
+                        elem: 'phone-link',
+                        raw: data.cell.replace(/\(|\)|\s|\-/g, ''),
+                        content: data.cell
+                    }
+                ]
             });
-            if (i !== order.length - 1) {
-                content.push(', ');
-            }
+        }
+
+        content.push({
+            elem: 'gap'
+        });
+
+        ['email', 'site'].filter((prop) => data[prop]).forEach(function(prop) {
+            content.push({
+                elem: prop,
+                data: data[prop]
+            });
+        });
+
+        content.push({
+            elem: 'gap'
+        });
+
+        ['skype', 'github', 'twitter'].filter((prop) => data[prop]).forEach(function(prop) {
+            content.push({
+                elem: prop,
+                content: data[prop]
+            });
         });
 
         return content;
-    })
-);
+    }),
 
-block('card').elem('country')(
-    tag()('span'),
-    attrs()({
-        itemprop: 'country-name'
-    })
-);
+    elem('phone-link')(
+        tag()('a'),
+        attrs()(function() {
+            return {
+                href: 'tel:' + this.ctx.raw
+            };
+        })
+    ),
 
-block('card').elem('city')(
-    tag()('span'),
-    attrs()({
-        itemprop: 'locality'
-    })
-);
-
-block('card').elem('zip')(
-    tag()('span'),
-    attrs()({
-        itemprop: 'postal-code'
-    })
-);
-
-block('card').elem('street-address')(
-    tag()('span'),
-    attrs()({
-        itemprop: 'street-address'
-    })
-);
-
-block('card').elem('contact').content()(function() {
-    var content = [];
-    var data = this.ctx.data.contact;
-
-    if (data.work) {
-        content.push({
-            elem: 'tel',
-            elemMods: { type: 'work' },
-            content: [
-                i18n[this.ctx.lang].tel,
-                {
-                    elem: 'phone-link',
-                    raw: data.work.replace(/\(|\)|\s|\-/g, ''),
-                    content: data.work
-                },
-                data.workExt
-                    ? i18n[this.ctx.lang].telExt + data.workExt
-                    : ''
-            ]
-        });
-    }
-
-    if (data.cell) {
-        content.push({
-            elem: 'tel',
-            elemMods: { type: 'cellular' },
-            content: [
-                i18n[this.ctx.lang].cell,
-                {
-                    elem: 'phone-link',
-                    raw: data.cell.replace(/\(|\)|\s|\-/g, ''),
-                    content: data.cell
-                }
-            ]
-        });
-    }
-
-    content.push({
-        elem: 'gap'
-    });
-
-    ['email', 'site'].filter((prop) => data[prop]).forEach(function(prop) {
-        content.push({
-            elem: prop,
-            data: data[prop]
-        });
-    });
-
-    content.push({
-        elem: 'gap'
-    });
-
-    ['skype', 'github', 'twitter'].filter((prop) => data[prop]).forEach(function(prop) {
-        content.push({
-            elem: prop,
-            content: data[prop]
-        });
-    });
-
-    return content;
-});
-
-block('card').elem('phone-link')(
-    tag()('a'),
-    attrs()(function() {
+    elem('site').content()(function() {
         return {
-            href: 'tel:' + this.ctx.raw
-        };
-    })
-);
-
-block('card').elem('site').content()(function() {
-    return {
-        elem: 'link',
-        attrs: {
-            itemprop: 'url'
-        },
-        url: this.ctx.data,
-        content: this.ctx.data.replace(/https?:\/\//, '')
-    };
-});
-
-block('card').elem('email').content()(function() {
-    return {
-        elem: 'link',
-        url: 'mailto:' + this.ctx.data,
-        content: this.ctx.data
-    };
-});
-
-block('card').elem('github').content()(function() {
-    return {
-        elem: 'link',
-        url: 'https://github.com/' + this.ctx.content,
-        content: 'github.com/' + this.ctx.content
-    };
-});
-
-block('card').elem('skype').content()(function() {
-    return [
-        'skype: ',
-        {
             elem: 'link',
-            url: 'skype:' + this.ctx.content + '?chat',
-            content: {
-                tag: 'span',
-                attrs: {
-                    itemprop: 'nickname'
-                },
-                content: this.ctx.content
-            }
-        }
-    ];
-});
-
-block('card').elem('twitter').content()(function() {
-    return {
-        elem: 'link',
-        url: 'https://twitter.com/' + this.ctx.content,
-        content: 'twitter.com/' + this.ctx.content
-    };
-});
-
-block('card').elem('link')(
-    tag()('a'),
-    attrs()(function() {
-        return {
-            href: this.ctx.url
+            attrs: {
+                itemprop: 'url'
+            },
+            url: this.ctx.data,
+            content: this.ctx.data.replace(/https?:\/\//, '')
         };
-    })
+    }),
+
+    elem('email').content()(function() {
+        return {
+            elem: 'link',
+            url: 'mailto:' + this.ctx.data,
+            content: this.ctx.data
+        };
+    }),
+
+    elem('github').content()(function() {
+        return {
+            elem: 'link',
+            url: 'https://github.com/' + this.ctx.content,
+            content: 'github.com/' + this.ctx.content
+        };
+    }),
+
+    elem('skype').content()(function() {
+        return [
+            'skype: ',
+            {
+                elem: 'link',
+                url: 'skype:' + this.ctx.content + '?chat',
+                content: {
+                    tag: 'span',
+                    attrs: {
+                        itemprop: 'nickname'
+                    },
+                    content: this.ctx.content
+                }
+            }
+        ];
+    }),
+
+    elem('twitter').content()(function() {
+        return {
+            elem: 'link',
+            url: 'https://twitter.com/' + this.ctx.content,
+            content: 'twitter.com/' + this.ctx.content
+        };
+    }),
+
+    elem('link')(
+        tag()('a'),
+        attrs()(function() {
+            return {
+                href: this.ctx.url
+            };
+        })
+    )
 );
--- blocks/page/page.bemhtml.js
+++ blocks/page/page.bemhtml.js
@@ -1,91 +1,92 @@
 /*global block,tag,attrs,mix,bem,match*/
-block('page').wrap()(function() {
-    return [
-        this.ctx.doctype || '<!DOCTYPE html>',
-        {
-            tag: 'html',
-            cls: 'ua_js_no',
-            content: [
-                {
-                    elem: 'head',
-                    content: [
-                        { tag: 'meta', attrs: { charset: 'utf-8' } },
-                        { tag: 'meta', attrs: { name: 'format-detection', content: 'telephone=no' } },
-                        { tag: 'title', content: this.ctx.title },
-                        {
-                            elem: 'js',
-                            content: '(function(e,c){e[c]=e[c].replace(/(ua_js_)no/g,"$1yes");})(document.documentElement,"className");'
-                        },
-                        {
-                            elem: 'js',
-                            content: '(function(){/Android|iPhone/i.test(navigator.userAgent)&&(document.documentElement.className += \' mobile\')})()'
-                        },
-                        this.ctx.head,
-                        this.ctx.favicon ? { elem: 'favicon', url: this.ctx.favicon } : ''
-                    ]
-                },
-                this.ctx
-            ]
-        }
-    ];
-});
-
 block('page')(
+    wrap()(function() {
+        return [
+            this.ctx.doctype || '<!DOCTYPE html>',
+            {
+                tag: 'html',
+                cls: 'ua_js_no',
+                content: [
+                    {
+                        elem: 'head',
+                        content: [
+                            { tag: 'meta', attrs: { charset: 'utf-8' } },
+                            { tag: 'meta', attrs: { name: 'format-detection', content: 'telephone=no' } },
+                            { tag: 'title', content: this.ctx.title },
+                            {
+                                elem: 'js',
+                                content: '(function(e,c){e[c]=e[c].replace(/(ua_js_)no/g,"$1yes");})(document.documentElement,"className");'
+                            },
+                            {
+                                elem: 'js',
+                                content: '(function(){/Android|iPhone/i.test(navigator.userAgent)&&(document.documentElement.className += \' mobile\')})()'
+                            },
+                            this.ctx.head,
+                            this.ctx.favicon ? { elem: 'favicon', url: this.ctx.favicon } : ''
+                        ]
+                    },
+                    this.ctx
+                ]
+            }
+        ];
+    }),
+
     tag()('body'),
+
     mix()(function() {
         return {
             block: 'font',
             mods: { face: 'textbook-new' }
         };
-    })
-);
+    }),
 
-block('page').elem('head')(
-    tag()('head'),
-    bem()(false)
-);
+    elem('head')(
+        tag()('head'),
+        bem()(false)
+    ),
 
-block('page').elem('meta')(
-    tag()('meta'),
-    bem()(false)
-);
+    elem('meta')(
+        tag()('meta'),
+        bem()(false)
+    ),
 
-block('page').elem('link')(
-    tag()('link'),
-    bem()(false)
-);
-
-block('page').elem('favicon')(
-    tag()('link'),
-    bem()(false),
-    attrs()(function() {
-        return {
-            rel: 'shortcut icon',
-            href: this.ctx.url
-        };
-    })
-);
-
-block('page').elem('js')(
-    tag()('script'),
-    bem()(false),
-    attrs()(function() {
-        return {
-            src: this.ctx.url
-        };
-    })
-);
-
-block('page').elem('css')(
-    tag()('style'),
-    bem()(false),
-    match(function() { return this.ctx.url; })(
+    elem('link')(
         tag()('link'),
+        bem()(false)
+    ),
+
+    elem('favicon')(
+        tag()('link'),
+        bem()(false),
         attrs()(function() {
             return {
-                rel: 'stylesheet',
+                rel: 'shortcut icon',
                 href: this.ctx.url
             };
         })
+    ),
+
+    elem('js')(
+        tag()('script'),
+        bem()(false),
+        attrs()(function() {
+            return {
+                src: this.ctx.url
+            };
+        })
+    ),
+
+    elem('css')(
+        tag()('style'),
+        bem()(false),
+        match(function() { return this.ctx.url; })(
+            tag()('link'),
+            attrs()(function() {
+                return {
+                    rel: 'stylesheet',
+                    href: this.ctx.url
+                };
+            })
+        )
     )
 );
